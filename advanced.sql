8b9bc045ec4c58b1e3da90445112777f05bb345d
AbdullahNasef <abdallahsayednasef2@gmail.com>
2026-05-13 19:32:51 +0300

    advanced.sql

---
+++ advanced.sql
@@ -0,0 +1,111 @@
+--CTE
+with clean_data as(
+	select 
+		InvoiceDate_F,
+        (quantity_F * UnitPrice_F) AS row_total
+from Fact_salse as f
+inner join Dim_Products as p
+		on f.StockCode_DP_F = p.StockCode_DP_F
+where f.quantity_F > 0 
+and f.UnitPrice_F> 0
+and P.Description_DP not in (
+    'DAMAGES', 'FAULTY', 'WRONGLY MARKED CARTON 22804', 
+    'MAILOUT', 'POSTAGE', 'DOTCOM POSTAGE', 'CRUK Commission',
+    'WEBSITE FIXED', 'FOR ONLINE RETAIL ORDERS',
+	'ALLOCATE STOCK FOR DOTCOM ORDERS TA'
+  )
+  and P.Description_DP not like '%ADJUST%'
+  and P.Description_DP not like '%STOCK%'
+)
+select
+	year(InvoiceDate_F) AS sales_year,
+	MONTH(InvoiceDate_F) AS sales_month,
+    SUM(row_total) AS Total_sales
+from clean_data
+group by year(InvoiceDate_F),MONTH(InvoiceDate_F)
+order by sales_year,sales_month;
+
+-- CTE + LAG + Growth Rate
+with monthlySales as(
+	select
+	year(f.InvoiceDate_F) as sales_year,
+	month(f.InvoiceDate_F) as sales_month,
+	sum(f.quantity_F * f.UnitPrice_F) as monthly_sales_current
+from Fact_salse as f
+inner join Dim_Products as p
+		on f.StockCode_DP_F = p.StockCode_DP_F
+where f.quantity_F > 0 
+and f.UnitPrice_F> 0
+and P.Description_DP not in (
+    'DAMAGES', 'FAULTY', 'WRONGLY MARKED CARTON 22804', 
+    'MAILOUT', 'POSTAGE', 'DOTCOM POSTAGE', 'CRUK Commission',
+    'WEBSITE FIXED', 'FOR ONLINE RETAIL ORDERS',
+	'ALLOCATE STOCK FOR DOTCOM ORDERS TA')
+group by year(f.InvoiceDate_F) , month(f.InvoiceDate_F)
+
+)
+select 
+	sales_year,sales_month,monthly_sales_current,
+	lag(monthly_sales_current) over (order by sales_year,sales_month ) as monthly_sales_previous,
+
+	format(((monthly_sales_current - lag(monthly_sales_current) over (order by sales_year,sales_month )) 
+	/ lag(monthly_sales_current) over (order by sales_year,sales_month )),'p') as growth_percentage
+from monthlySales
+order by sales_year,sales_month;
+
+----CTE + LAG + Growth Rate for specific product (EX. TOP 20)
+with ProductMonthlySales as(
+	select
+	p.Description_DP,
+	year(f.InvoiceDate_F) as sales_year,
+	month(f.InvoiceDate_F) as sales_month,
+	sum(f.quantity_F * f.UnitPrice_F) as monthly_sales_current
+from Fact_salse as f
+inner join Dim_Products as p
+		on f.StockCode_DP_F = p.StockCode_DP_F
+where f.quantity_F > 0 
+	and f.UnitPrice_F> 0
+	and p.Description_DP = 'REGENCY CAKESTAND 3 TIER'
+group by year(f.InvoiceDate_F) , month(f.InvoiceDate_F),p.Description_DP
+
+)
+select 
+	sales_year,sales_month,Description_DP,monthly_sales_current,
+	lag(monthly_sales_current) over (order by sales_year,sales_month ) as monthly_sales_previous,
+
+	format(((monthly_sales_current - lag(monthly_sales_current) over (order by sales_year,sales_month )) 
+	/ lag(monthly_sales_current) over (order by sales_year,sales_month )),'p') as growth_percentage
+from ProductMonthlySales
+order by sales_year,sales_month;
+
+--CTE + Identify top three products in each country
+WITH 
+ProductSalesByCountry AS (
+    SELECT 
+        c.country_DC,
+        p.Description_DP,
+        SUM(f.quantity_F * f.UnitPrice_F) AS Total_Sales
+    FROM Fact_salse AS f
+    INNER JOIN Dim_Customers AS c ON f.Customerid_DC_F = c.Customerid_DC_F
+    INNER JOIN Dim_Products AS p ON f.StockCode_DP_F = p.StockCode_DP_F
+    WHERE f.quantity_F > 0 AND f.UnitPrice_F > 0
+	and P.Description_DP not in (
+    'DAMAGES', 'FAULTY', 'WRONGLY MARKED CARTON 22804', 
+    'MAILOUT', 'POSTAGE', 'DOTCOM POSTAGE', 'CRUK Commission',
+    'WEBSITE FIXED', 'FOR ONLINE RETAIL ORDERS',
+	'ALLOCATE STOCK FOR DOTCOM ORDERS TA')
+    GROUP BY c.country_DC, p.Description_DP
+),
+RankedProducts AS (
+    SELECT 
+        country_DC,
+        Description_DP,
+        Total_Sales,
+
+        DENSE_RANK() OVER (PARTITION BY country_DC ORDER BY Total_Sales DESC) AS Product_Rank
+    FROM ProductSalesByCountry
+)
+
+SELECT * FROM RankedProducts
+WHERE Product_Rank <= 3
+ORDER BY country_DC, Product_Rank;

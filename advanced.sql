35c697203d86f1cad64c31a48ba1f2c5de4c48fc
AbdullahNasef <abdallahsayednasef2@gmail.com>
2026-05-14 00:30:53 +0300

    Update advanced.sql

--- advanced.sql
+++ advanced.sql
@@ -109,3 +109,129 @@ RankedProducts AS (
 SELECT * FROM RankedProducts
 WHERE Product_Rank <= 3
 ORDER BY country_DC, Product_Rank;
+
+
+
+-- view
+create view v_cleanData as 
+select 
+	f.InvoiceNo_F,
+	f.InvoiceDate_F,
+	p.Description_DP,
+	(f.quantity_F * f.UnitPrice_F) as rowtotal
+from Fact_salse f
+inner join Dim_Products p
+	on f.StockCode_DP_F = p.StockCode_DP_F
+WHERE f.quantity_F > 0 AND f.UnitPrice_F > 0
+	and P.Description_DP not in (
+    'DAMAGES', 'FAULTY', 'WRONGLY MARKED CARTON 22804', 
+    'MAILOUT', 'POSTAGE', 'DOTCOM POSTAGE', 'CRUK Commission',
+    'WEBSITE FIXED', 'FOR ONLINE RETAIL ORDERS',
+	'ALLOCATE STOCK FOR DOTCOM ORDERS TA')
+	 and P.Description_DP not like '%ADJUST%'
+     and P.Description_DP not like '%STOCK%' ;
+
+--أغلى 10 عمليات بيع منفردة
+select top 10 
+	Description_DP,rowtotal
+from v_cleanData
+order by rowtotal desc;
+
+
+-- في عدد سطور اقل و اسرع  joinو كدا نقدر نكتب الاكواد بتاع 
+--view مثلا :اعلى 10 منتجات من حيث المبيعات باستخدام ال  <<<---
+select top 10 
+	Description_DP,sum(rowtotal) as totalSales
+from v_cleanData
+group by Description_DP
+order by totalSales desc;
+
+--- Window Function مع  CTE ب ال  view  ربط ال 
+with monthelyRate as (
+	select 
+	year(InvoiceDate_F) as salse_year,
+	month(InvoiceDate_F) as salse_month,
+	sum(rowtotal) as monthelysum
+	from v_cleanData
+	group by year(InvoiceDate_F) , month(InvoiceDate_F)
+)
+
+select salse_year, salse_month,monthelysum,
+	lag(monthelysum)over (order by salse_year, salse_month) as previes_month,
+	format(((monthelysum - lag(monthelysum)over (order by salse_year, salse_month))/lag(monthelysum)over (order by salse_year, salse_month)), 'p') as growth_percentage
+from monthelyRate;
+
+---- شوف الفرق بين التالي و السابق و شاهد الفرق
+with monthlySales as(
+	select
+	year(f.InvoiceDate_F) as sales_year,
+	month(f.InvoiceDate_F) as sales_month,
+	sum(f.quantity_F * f.UnitPrice_F) as monthly_sales_current
+from Fact_salse as f
+inner join Dim_Products as p
+		on f.StockCode_DP_F = p.StockCode_DP_F
+where f.quantity_F > 0 
+and f.UnitPrice_F> 0
+and P.Description_DP not in (
+    'DAMAGES', 'FAULTY', 'WRONGLY MARKED CARTON 22804', 
+    'MAILOUT', 'POSTAGE', 'DOTCOM POSTAGE', 'CRUK Commission',
+    'WEBSITE FIXED', 'FOR ONLINE RETAIL ORDERS',
+	'ALLOCATE STOCK FOR DOTCOM ORDERS TA')
+and P.Description_DP not like '%ADJUST%'
+and P.Description_DP not like '%STOCK%' 
+group by year(f.InvoiceDate_F) , month(f.InvoiceDate_F)
+
+)
+select 
+	sales_year,sales_month,monthly_sales_current,
+	lag(monthly_sales_current) over (order by sales_year,sales_month ) as monthly_sales_previous,
+
+	format(((monthly_sales_current - lag(monthly_sales_current) over (order by sales_year,sales_month )) 
+	/ lag(monthly_sales_current) over (order by sales_year,sales_month )),'p') as growth_percentage
+from monthlySales
+order by sales_year,sales_month;
+
+---Subquery
+-- الفواتير اللي تخطت المتوسط
+select 
+	InvoiceNo_F,rowtotal
+from v_cleanData
+where rowtotal > (select avg(rowtotal) from v_cleanData );
+
+-- تقسيم الفواتير لفئات 
+with avgvalue as(
+	select 
+	InvoiceNo_F,rowtotal
+from v_cleanData
+where rowtotal > (select avg(rowtotal) from v_cleanData )
+)
+select *,ntile(4) over(order by rowtotal desc) as sales_cat
+from avgvalue;
+-- اجمالي مبيعات الفئة 1
+with 
+	avgvalue as(
+		select 
+		InvoiceNo_F,rowtotal
+	from v_cleanData
+	where rowtotal > (select avg(rowtotal) from v_cleanData )
+	),
+	Cat_Sales as(
+	select rowtotal,ntile(4) over(order by rowtotal desc) as sales_cat
+	from avgvalue
+	)
+
+select sum(rowtotal)
+from Cat_Sales
+where sales_cat = 1;
+
+-- whereمع  havingاستخدام 
+-- الدول التي تجاوزت مبيعاتها 50 ألف دولار مع استبعاد المعاملات الأقل من 100 دولار
+select 
+	c.country_DC,
+	sum(f.quantity_F * f.UnitPrice_F) as totalSales
+from Fact_salse f
+inner join Dim_Customers c
+	on f.Customerid_DC_F = c.Customerid_DC_F
+where (f.quantity_F * f.UnitPrice_F) > 100
+group by c.country_DC
+having sum(f.quantity_F * f.UnitPrice_F) > 50000;
